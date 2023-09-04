09175869b7d49016873b835d657bffdc0c6206d5
Eric Undersander <eundersander@meta.com>
2023-09-04 13:34:43 -0400

    expose network IP address and port to editor; refactor XR input helper

--- Assets/Scripts/NetworkClient.cs
+++ Assets/Scripts/NetworkClient.cs
@@ -3,214 +3,25 @@ using UnityEngine;
 
 using NativeWebSocket;
 using UnityEngine.Assertions;
-using System.Collections.Generic;
-using UnityEngine.InputSystem;
-using System.Collections;
-using static UnityEngine.InputSystem.HID.HID;
-using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
-
-public class BoolArrayHelper
-{
-    public static List<int> GetTrueIndices(bool[] arr)
-    {
-        List<int> indices = new List<int>();
-        for (int i = 0; i < arr.Length; i++)
-        {
-            if (arr[i])
-            {
-                indices.Add(i);
-            }
-        }
-        return indices;
-    }
-}
-
-[Serializable]
-public class ButtonInputData
-{
-    public List<int> buttonHeld = new List<int>();
-    public List<int> buttonUp = new List<int>();
-    public List<int> buttonDown = new List<int>();
-}
-
-public class XRInputHelper
-{
-    XRIDefaultInputActions _inputActions;
-    ButtonInputData _inputData = new ButtonInputData();
-    const int NUM_BUTTONS = 4;
-    bool[] _buttonHeld = new bool[4];
-
-    public XRInputHelper()
-    {
-        for (int buttonId = 0; buttonId < NUM_BUTTONS; buttonId++)
-        {
-            _buttonHeld[buttonId] = false;
-        }
-        OnEndFrame();
-
-        _inputActions = new XRIDefaultInputActions();
-        _inputActions.Enable();
-        _inputActions.XRILeftHandInteraction.Activate.performed += LeftActivateCallback;
-        _inputActions.XRILeftHandInteraction.Activate.canceled += LeftActivateCallback;
-        _inputActions.XRILeftHandInteraction.Select.performed += LeftSelectCallback;
-        _inputActions.XRILeftHandInteraction.Select.canceled += LeftSelectCallback;
-        _inputActions.XRIRightHandInteraction.Activate.performed += RightActivateCallback;
-        _inputActions.XRIRightHandInteraction.Activate.canceled += RightActivateCallback;
-        _inputActions.XRIRightHandInteraction.Select.performed += RightSelectCallback;
-        _inputActions.XRIRightHandInteraction.Select.canceled += RightSelectCallback;
-    }
-
-    private void ButtonPressReleaseCallback(int buttonId, bool down)
-    {
-        Assert.IsTrue(buttonId >= 0 && buttonId < NUM_BUTTONS);
-        Debug.Log($"ButtonPressReleaseCallback {buttonId} {down}");
-        if (down)
-        {
-            if (!_buttonHeld[buttonId])
-            {
-                _buttonHeld[buttonId] = true;
-                _inputData.buttonDown.Add(buttonId);
-            }
-        }
-        else
-        {
-            if (_buttonHeld[buttonId])
-            {
-                _buttonHeld[buttonId] = false;
-                _inputData.buttonUp.Add(buttonId);
-            }
-        }
-    }
-
-    public ButtonInputData UpdateInputData()
-    {
-        _inputData.buttonHeld = BoolArrayHelper.GetTrueIndices(_buttonHeld);
-        return _inputData;
-    }
-    public void OnEndFrame()
-    {
-        for (int buttonId = 0; buttonId < NUM_BUTTONS; buttonId++)
-        {
-            _inputData.buttonUp.Clear();
-            _inputData.buttonDown.Clear();
-        }
-    }
-
-    private void LeftActivateCallback(UnityEngine.InputSystem.InputAction.CallbackContext obj)
-    {
-        ButtonPressReleaseCallback(0, obj.performed);
-    }
-
-    private void LeftSelectCallback(UnityEngine.InputSystem.InputAction.CallbackContext obj)
-    {
-        ButtonPressReleaseCallback(1, obj.performed);
-    }
-
-    private void RightActivateCallback(InputAction.CallbackContext obj)
-    {
-        ButtonPressReleaseCallback(2, obj.performed);
-    }
-
-    private void RightSelectCallback(InputAction.CallbackContext obj)
-    {
-        ButtonPressReleaseCallback(3, obj.performed);
-    }
-}
-
-public class CoordinateConventionHelper
-{
-    private static Quaternion _defaultRotation = Quaternion.Euler(0, 180, 0);
-    private static Quaternion _invDefaultRotation = Quaternion.Inverse(_defaultRotation);
-
-
-    public static Vector3 ToUnityVector(List<float> translation)
-    {
-        return new Vector3(
-            translation[0],
-            translation[1],
-            -translation[2]
-        );
-    }
-
-    public static Quaternion ToUnityQuaternion(List<float> rotation)
-    {
-        Quaternion newRot = new Quaternion(
-            rotation[1],
-            -rotation[2],
-            -rotation[3],
-            rotation[0]
-        );
-
-        newRot = _defaultRotation * newRot;
-        return newRot;
-    }
-
-    public static List<float> ToHabitatVector(Vector3 translation)
-    {
-        return new List<float>
-        {
-            translation.x,
-            translation.y,
-            -translation.z
-        };
-    }
-
-    public static List<float> ToHabitatQuaternion(Quaternion rotation)
-    {
-        Quaternion convertedRotation = _invDefaultRotation * rotation;
-
-        return new List<float>
-        {
-            convertedRotation.w,
-            convertedRotation.x,
-            -convertedRotation.y,
-            -convertedRotation.z
-        };
-    }
-}
 
 [Serializable]
 public class ClientState
 {
     public AvatarData avatar = new AvatarData();
-    public ButtonInputData input; //  = new ButtonInputData();
-}
-
-[System.Serializable]
-public class AvatarData
-{
-    public PoseData root = new PoseData();
-    public PoseData[] hands = new PoseData[]
-    {
-        new PoseData(),
-        new PoseData()
-    };
-}
-
-[System.Serializable]
-public class PoseData
-{
-    public List<float> position = new List<float>(3);
-    public List<float> rotation = new List<float>(4);
-
-    public void FromGameObject(GameObject gameObject)
-    {
-        position = CoordinateConventionHelper.ToHabitatVector(gameObject.transform.position);
-        rotation = CoordinateConventionHelper.ToHabitatQuaternion(gameObject.transform.rotation);
-    }
+    public ButtonInputData input;
 }
 
 public class NetworkClient : MonoBehaviour
 {
-    //Avatar avatar; // Assuming createAvatar() returns an Avatar instance
-    //InputManager inputMgr; // Assuming InputManager is defined elsewhere
+    public string serverAddress; //  = "192.168.4.58";
+    public string serverPort; // = "8888";
 
     public GameObject vrHeadsetObject;
     public GameObject vrLeftControllerObject;
     public GameObject vrRightControllerObject;
 
     string wsProtocol;
-    string serverAddress;
+    string _fullServerAddress;
 
     WebSocket websocket;
 
@@ -226,12 +37,9 @@ public class NetworkClient : MonoBehaviour
 
         _xrInputHelper = new XRInputHelper();
 
-        //avatar = CreateAvatar(); // Assuming CreateAvatar() method exists
-        //inputMgr = new InputManager(); // Assuming InputManager is defined elsewhere
-
-        bool isHttps = false; // Application.absoluteURL.StartsWith("https:");
-        wsProtocol = isHttps ? "wss://" : "ws://";
-        serverAddress = $"{wsProtocol}192.168.4.58:8888";
+        bool isHttps = false;
+        wsProtocol = isHttps ? "wss" : "ws";
+        _fullServerAddress = $"{wsProtocol}://{serverAddress}:{serverPort}";
 
         SetConnectionState(false);
 
@@ -242,25 +50,6 @@ public class NetworkClient : MonoBehaviour
 
         // Keep sending messages at every 0.1s
         InvokeRepeating("SendClientState", 0.0f, 0.1f);
-
-        TryEnableXRDeviceSimulatorForEditor();
-    }
-
-    private void TryEnableXRDeviceSimulatorForEditor()
-    {
-#if UNITY_EDITOR
-        XRDeviceSimulator xrDeviceSimulator = (XRDeviceSimulator)FindObjectOfType(typeof(XRDeviceSimulator), true);
-
-        // GameObject xrDeviceSimulator = GameObject.Find("XR Device Simulator");
-        if (xrDeviceSimulator != null)
-        {
-            xrDeviceSimulator.gameObject.SetActive(true);
-        }
-        else
-        {
-            Debug.LogWarning("XR Device Simulator not found in the scene!");
-        }
-#endif
     }
 
     void Update()
@@ -275,7 +64,7 @@ public class NetworkClient : MonoBehaviour
 
     async void ConnectWebSocket()
     {
-        websocket = new WebSocket(serverAddress);
+        websocket = new WebSocket(_fullServerAddress);
 
         websocket.OnOpen += () =>
         {
@@ -303,9 +92,7 @@ public class NetworkClient : MonoBehaviour
 
         websocket.OnMessage += (bytes) =>
         {
-            // Debug.Log("OnMessage!");
             string message = System.Text.Encoding.UTF8.GetString(bytes);
-            // Debug.Log("OnMessage! " + message);
 
             ProcessReceivedKeyframes(message);
         };
@@ -367,22 +154,8 @@ public class NetworkClient : MonoBehaviour
         CancelInvoke("AttemptReconnection");
     }
 
-    // Define your own implementation of these methods
-    //Avatar CreateAvatar() { /* ... */ }
     void SetConnectionState(bool connected)
     {
         Debug.Log($"SetConnectionState: {connected}");
     }
-    //void DeleteAllInstancesFromKeyframes() { /* ... */ }
-}
-
-[Serializable]
-public class Avatar
-{
-    // Define properties and methods relevant to Avatar here
-}
-
-public class InputManager
-{
-    // Define properties and methods relevant to InputManager here
 }
