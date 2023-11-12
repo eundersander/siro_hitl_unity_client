18eaf86105a0f411ab65db2031e04ff1d0236801
Mikael Dallaire Cote <mdcote@fb.com>
2023-11-11 21:52:18 -0500

    Update font material so that it renders on top of everything. Make text billboard smoother.

--- Assets/Scripts/TextRenderer.cs
+++ Assets/Scripts/TextRenderer.cs
@@ -21,23 +21,23 @@ public class TextRenderer : MonoBehaviour
 
     void Update()
     {
-        // Update billboard if text is being displayed
+        Camera camera = Camera.main;
+        _targetTransform.transform.position = camera.transform.position + camera.transform.forward * uiPlaneDistance;
+        _targetTransform.transform.LookAt(camera.transform, Vector3.up);
+        _targetTransform.transform.Rotate(Vector3.up, 180.0f, Space.Self);
+
         if (textPanelRoot.activeSelf)
         {
-            UpdateBillboard();
             textPanelRoot.transform.position = 
                 Vector3.Lerp(textPanelRoot.transform.position, _targetTransform.position, Time.deltaTime * UI_GAZE_FOLLOWING_SPEED);
             textPanelRoot.transform.rotation =
                 Quaternion.Slerp(textPanelRoot.transform.rotation, _targetTransform.rotation, Time.deltaTime * UI_GAZE_FOLLOWING_SPEED);
         }
-    }
-
-    void UpdateBillboard()
-    {
-        Camera camera = Camera.main;
-        _targetTransform.transform.position = camera.transform.position + camera.transform.forward * uiPlaneDistance;
-        _targetTransform.transform.LookAt(camera.transform, Vector3.up);
-        _targetTransform.transform.Rotate(Vector3.up, 180.0f, Space.Self);
+        else 
+        {
+            textPanelRoot.transform.position = _targetTransform.position;
+            textPanelRoot.transform.rotation = _targetTransform.rotation;
+        }
     }
 
     public void SetText(string text)
@@ -46,12 +46,5 @@ public class TextRenderer : MonoBehaviour
 
         textPanelRoot.SetActive(!text.IsNullOrEmpty());
         textComponent.text = text;
-
-        if (textPanelRoot.activeSelf)
-        {
-            UpdateBillboard();
-            textPanelRoot.transform.position = _targetTransform.position;
-            textPanelRoot.transform.rotation = _targetTransform.rotation;
-        }
     }
 }
