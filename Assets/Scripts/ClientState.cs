192cc4fc9f445de6d2fac0e538e0ba24ab833d2f
Eric Undersander <eundersander@meta.com>
2024-12-18 04:30:43 -0600

    add ArticulatedHandData and add hand-tracking to InputTrackerXRPose

--- Assets/Scripts/ClientState.cs
+++ Assets/Scripts/ClientState.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [Serializable]
 // Message that is sent from the client to the server periodically.
@@ -13,15 +14,84 @@ public class ClientState
 }
 
 [Serializable]
+public class ArticulatedHandData
+{
+    // https://registry.khronos.org/OpenXR/specs/1.0/html/xrspec.html#convention-of-hand-joints
+    // typedef enum XrHandJointEXT {
+    //     XR_HAND_JOINT_PALM_EXT = 0,
+    //     XR_HAND_JOINT_WRIST_EXT = 1,
+    //     XR_HAND_JOINT_THUMB_METACARPAL_EXT = 2,
+    //     XR_HAND_JOINT_THUMB_PROXIMAL_EXT = 3,
+    //     XR_HAND_JOINT_THUMB_DISTAL_EXT = 4,
+    //     XR_HAND_JOINT_THUMB_TIP_EXT = 5,
+    //     XR_HAND_JOINT_INDEX_METACARPAL_EXT = 6,
+    //     XR_HAND_JOINT_INDEX_PROXIMAL_EXT = 7,
+    //     XR_HAND_JOINT_INDEX_INTERMEDIATE_EXT = 8,
+    //     XR_HAND_JOINT_INDEX_DISTAL_EXT = 9,
+    //     XR_HAND_JOINT_INDEX_TIP_EXT = 10,
+    //     XR_HAND_JOINT_MIDDLE_METACARPAL_EXT = 11,
+    //     XR_HAND_JOINT_MIDDLE_PROXIMAL_EXT = 12,
+    //     XR_HAND_JOINT_MIDDLE_INTERMEDIATE_EXT = 13,
+    //     XR_HAND_JOINT_MIDDLE_DISTAL_EXT = 14,
+    //     XR_HAND_JOINT_MIDDLE_TIP_EXT = 15,
+    //     XR_HAND_JOINT_RING_METACARPAL_EXT = 16,
+    //     XR_HAND_JOINT_RING_PROXIMAL_EXT = 17,
+    //     XR_HAND_JOINT_RING_INTERMEDIATE_EXT = 18,
+    //     XR_HAND_JOINT_RING_DISTAL_EXT = 19,
+    //     XR_HAND_JOINT_RING_TIP_EXT = 20,
+    //     XR_HAND_JOINT_LITTLE_METACARPAL_EXT = 21,
+    //     XR_HAND_JOINT_LITTLE_PROXIMAL_EXT = 22,
+    //     XR_HAND_JOINT_LITTLE_INTERMEDIATE_EXT = 23,
+    //     XR_HAND_JOINT_LITTLE_DISTAL_EXT = 24,
+    //     XR_HAND_JOINT_LITTLE_TIP_EXT = 25,
+    //     XR_HAND_JOINT_MAX_ENUM_EXT = 0x7FFFFFFF
+    // } XrHandJointEXT;
+
+    public const int NumHandBones = 26;
+    public const int NumFloatsPerPosition = 3;
+    public const int NumFloatsPerRotation = 4;
+    public float[] positions = new float[NumHandBones * NumFloatsPerPosition];
+    public float[] rotations = new float[NumHandBones * NumFloatsPerRotation];
+
+    public void FromGameObjectList(GameObject[] gameObjects)
+    {
+        if (gameObjects.Length != NumHandBones)
+        {
+            throw new ArgumentException($"Expected {NumHandBones} game objects, but received {gameObjects.Length}.");
+        }
+
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            GameObject gameObject = gameObjects[i];
+
+            // Get position and rotation as float arrays
+            float[] position = CoordinateSystem.ToHabitatVector(gameObject.transform.position).ToArray();
+            float[] rotation = CoordinateSystem.ToHabitatQuaternion(gameObject.transform.rotation).ToArray();
+
+            // Copy position data into the positions array
+            int positionIndex = i * NumFloatsPerPosition;
+            positions[positionIndex] = position[0];
+            positions[positionIndex + 1] = position[1];
+            positions[positionIndex + 2] = position[2];
+
+            // Copy rotation data into the rotations array
+            int rotationIndex = i * NumFloatsPerRotation;
+            rotations[rotationIndex] = rotation[0];
+            rotations[rotationIndex + 1] = rotation[1];
+            rotations[rotationIndex + 2] = rotation[2];
+            rotations[rotationIndex + 3] = rotation[3];
+        } 
+    } 
+};
+
+
+[Serializable]
 // Contains the avatar head and controller poses.
 public class AvatarData
 {
     public PoseData root = new PoseData();
-    public PoseData[] hands = new PoseData[]
-    {
-        new PoseData(),
-        new PoseData()
-    };
+    public PoseData[] hands = Enumerable.Range(0, 2).Select(_ => new PoseData()).ToArray();
+    public ArticulatedHandData[] articulatedHands = Enumerable.Range(0, 2).Select(_ => new ArticulatedHandData()).ToArray();    
 }
 
 [Serializable]
--- Assets/Scripts/InputTrackerXRPose.cs
+++ Assets/Scripts/InputTrackerXRPose.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public class InputTrackerXRPose : InputTracker
 {
@@ -7,14 +9,126 @@ public class InputTrackerXRPose : InputTracker
     public GameObject xrHeadObject;
     public GameObject xrLeftControllerObject;
     public GameObject xrRightControllerObject;
+    public Transform leftHandRigRoot;
+    public Transform rightHandRigRoot;
+
+    private GameObject[] _leftHandBoneObjects = null;
+    private GameObject[] _rightHandBoneObjects = null;
 
     public override void UpdateClientState(ref ClientState state)
     {
         _inputData.root.FromGameObject(xrHeadObject);
         _inputData.hands[0].FromGameObject(xrLeftControllerObject);
         _inputData.hands[1].FromGameObject(xrRightControllerObject);
+
+        //if(_leftHandBoneObjects == null)
+        //{
+        //    _leftHandBoneObjects = ConstructFlatBoneObjectList(leftHandRigRoot).ToArray();
+        //    _rightHandBoneObjects = ConstructFlatBoneObjectList(rightHandRigRoot).ToArray();
+        //}
+        // _inputData.articulatedHands[0].FromGameObjectList(_leftHandBoneObjects);
+        // _inputData.articulatedHands[1].FromGameObjectList(_rightHandBoneObjects);
+
         state.avatar = _inputData;
     }
+
+    // https://registry.khronos.org/OpenXR/specs/1.0/html/xrspec.html#convention-of-hand-joints
+    // typedef enum XrHandJointEXT {
+    //     XR_HAND_JOINT_PALM_EXT = 0,
+    //     XR_HAND_JOINT_WRIST_EXT = 1,
+    //     XR_HAND_JOINT_THUMB_METACARPAL_EXT = 2,
+    //     XR_HAND_JOINT_THUMB_PROXIMAL_EXT = 3,
+    //     XR_HAND_JOINT_THUMB_DISTAL_EXT = 4,
+    //     XR_HAND_JOINT_THUMB_TIP_EXT = 5,
+    //     XR_HAND_JOINT_INDEX_METACARPAL_EXT = 6,
+    //     XR_HAND_JOINT_INDEX_PROXIMAL_EXT = 7,
+    //     XR_HAND_JOINT_INDEX_INTERMEDIATE_EXT = 8,
+    //     XR_HAND_JOINT_INDEX_DISTAL_EXT = 9,
+    //     XR_HAND_JOINT_INDEX_TIP_EXT = 10,
+    //     XR_HAND_JOINT_MIDDLE_METACARPAL_EXT = 11,
+    //     XR_HAND_JOINT_MIDDLE_PROXIMAL_EXT = 12,
+    //     XR_HAND_JOINT_MIDDLE_INTERMEDIATE_EXT = 13,
+    //     XR_HAND_JOINT_MIDDLE_DISTAL_EXT = 14,
+    //     XR_HAND_JOINT_MIDDLE_TIP_EXT = 15,
+    //     XR_HAND_JOINT_RING_METACARPAL_EXT = 16,
+    //     XR_HAND_JOINT_RING_PROXIMAL_EXT = 17,
+    //     XR_HAND_JOINT_RING_INTERMEDIATE_EXT = 18,
+    //     XR_HAND_JOINT_RING_DISTAL_EXT = 19,
+    //     XR_HAND_JOINT_RING_TIP_EXT = 20,
+    //     XR_HAND_JOINT_LITTLE_METACARPAL_EXT = 21,
+    //     XR_HAND_JOINT_LITTLE_PROXIMAL_EXT = 22,
+    //     XR_HAND_JOINT_LITTLE_INTERMEDIATE_EXT = 23,
+    //     XR_HAND_JOINT_LITTLE_DISTAL_EXT = 24,
+    //     XR_HAND_JOINT_LITTLE_TIP_EXT = 25,
+    //     XR_HAND_JOINT_MAX_ENUM_EXT = 0x7FFFFFFF
+    // } XrHandJointEXT;
+
+    private static readonly string[] BoneRegexPatterns = new string[]
+    {
+        ".*palm.*",
+        ".*wrist.*",
+        ".*thumb.*metacarpal.*",
+        ".*thumb.*proximal.*",
+        ".*thumb.*distal.*",
+        ".*thumb.*tip.*",
+        ".*index.*metacarpal.*",
+        ".*index.*proximal.*",
+        ".*index.*intermediate.*",
+        ".*index.*distal.*",
+        ".*index.*tip.*",
+        ".*middle.*metacarpal.*",
+        ".*middle.*proximal.*",
+        ".*middle.*intermediate.*",
+        ".*middle.*distal.*",
+        ".*middle.*tip.*",
+        ".*ring.*metacarpal.*",
+        ".*ring.*proximal.*",
+        ".*ring.*intermediate.*",
+        ".*ring.*distal.*",
+        ".*ring.*tip.*",
+        ".*little.*metacarpal.*",
+        ".*little.*proximal.*",
+        ".*little.*intermediate.*",
+        ".*little.*distal.*",
+        ".*little.*tip.*"
+    };
+
+    private static List<GameObject> ConstructFlatBoneObjectList(Transform rigRoot)
+    {
+        List<GameObject> orderedBones = new List<GameObject>(new GameObject[ArticulatedHandData.NumHandBones]);
+        Transform[] allChildren = rigRoot.GetComponentsInChildren<Transform>(true);
+
+        Debug.Log("All children under rigRoot:");
+        foreach (Transform child in allChildren)
+        {
+            Debug.Log(child.name);
+        }
+
+        for (int i = 0; i < BoneRegexPatterns.Length; i++)
+        {
+            string pattern = BoneRegexPatterns[i];
+            foreach (Transform child in allChildren)
+            {
+                if (System.Text.RegularExpressions.Regex.IsMatch(child.name, pattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                {
+                    orderedBones[i] = child.gameObject;
+                    break;
+                }
+            }
+        }
+
+        // Ensure no null entries remain in the list
+        for (int i = 0; i < orderedBones.Count; i++)
+        {
+            if (orderedBones[i] == null)
+            {
+                throw new System.Exception($"Missing bone for pattern: {BoneRegexPatterns[i]} in rig: {rigRoot.name}");
+            }
+        }
+
+        return orderedBones;
+    }
+    
 
     public override void OnEndFrame() {}
 }
